4dbdbf67fcb2d9f80085d0e2a7964141eae7bc45
Eduardo Szeckir <eszeckirjr@gmail.com>
2022-06-28 14:45:37 -0700

    Adding vega vault route

--- src/App.js
+++ src/App.js
@@ -1,47 +1,45 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {useContext} from 'react';
-import HomePageLayout from './components/templates/HomePageLayout.js';
-import BlogPageLayout from './components/templates/BlogPageLayout.js';
-import UserRegistrationPageLayout from './components/templates/UserRegistrationPageLayout.js'
-import SimplePageLayout from './components/templates/SimplePageLayout.js'
-import { Route, Switch } from 'react-router-dom';
-import Platform from './components/pages/Platform.js';
-import Login from './components/pages/Login.js';
-import NewsAndEvents from './components/pages/NewsAndEvents.js';
-import Resources from './components/pages/Resources.js';
-import AdminPanel from './components/pages/AdminPanel.js';
-import Leadership from './components/pages/Leadership.js';
-import UserRegistration from './components/pages/UserRegistration.js';
-import { UserProvider } from './auth/UserProvider.js';
-import ContactUsPageLayout from './components/templates/ContactUsPageLayout.js'
-import ContactUs from './components/pages/ContactUs.js';
-import { UserContext } from './auth/UserProvider.js';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useContext } from "react";
+import HomePageLayout from "./components/templates/HomePageLayout.js";
+import BlogPageLayout from "./components/templates/BlogPageLayout.js";
+import UserRegistrationPageLayout from "./components/templates/UserRegistrationPageLayout.js";
+import SimplePageLayout from "./components/templates/SimplePageLayout.js";
+import { Route, Switch } from "react-router-dom";
+import Platform from "./components/pages/Platform.js";
+import Login from "./components/pages/Login.js";
+import NewsAndEvents from "./components/pages/NewsAndEvents.js";
+import Resources from "./components/pages/Resources.js";
+import AdminPanel from "./components/pages/AdminPanel.js";
+import Leadership from "./components/pages/Leadership.js";
+import UserRegistration from "./components/pages/UserRegistration.js";
+import { UserProvider } from "./auth/UserProvider.js";
+import ContactUsPageLayout from "./components/templates/ContactUsPageLayout.js";
+import ContactUs from "./components/pages/ContactUs.js";
+import { UserContext } from "./auth/UserProvider.js";
 
-import UserAccount from './components/pages/UserAccount.js';
+import UserAccount from "./components/pages/UserAccount.js";
+import { VegaVaultPage } from "./components/pages/VegaVault";
 
 function App() {
-  
-
-  const {user, setUserInfo, logout} = useContext(UserContext);
+  const { user, setUserInfo, logout } = useContext(UserContext);
 
   return (
-   <UserProvider value ={user, setUserInfo, logout}> 
-        <Switch>
-        	<Route path="/" component={HomePageLayout} exact />
-            <Route path="/contactus" component={ContactUs} exact />
-        	<Route path="/leadership" component={Leadership} exact />
-        	<Route path="/news" component={NewsAndEvents} />
-        	<Route path="/platform" component={Platform} />
-        	<Route path="/login" component={Login} />
-          <Route path="/account" component={UserAccount} />
-          <Route path="/resources" component={Resources} />
-          <Route path="/adminpanel" component={AdminPanel} />
-        </Switch>
+    <UserProvider value={(user, setUserInfo, logout)}>
+      <Switch>
+        <Route path="/contactus" component={ContactUs} exact />
+        <Route path="/leadership" component={Leadership} exact />
+        <Route path="/news" component={NewsAndEvents} exact />
+        <Route path="/platform" component={Platform} exact />
+        <Route path="/login" component={Login} exact />
+        <Route path="/account" component={UserAccount} exact />
+        <Route path="/resources" component={Resources} exact />
+        <Route path="/adminpanel" component={AdminPanel} exact />
+        <Route path="/vegavault" component={VegaVaultPage} exact />
+        <Route path="/" component={HomePageLayout} />
+      </Switch>
     </UserProvider>
   );
 }
-
-
 
 export default App;
